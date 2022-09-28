ceb804f1c7a462a5f9e50e82041e8d80964372e4
Angelito Salud <asalud@you-source.com>
2022-09-28 15:35:55 +0800

    Add balnk unit test

---
+++ test/steps/navigate-to-page.ts
@@ -0,0 +1,16 @@
+import { Struct } from 'google-protobuf/google/protobuf/struct_pb';
+import * as chai from 'chai';
+import { default as sinon } from 'ts-sinon';
+import * as sinonChai from 'sinon-chai';
+import 'mocha';
+
+chai.use(sinonChai);
+
+describe('NavigateToPage', () => {
+  const expect = chai.expect;
+  beforeEach(() => {
+  });
+  it('should return expected step metadata', () => {
+    expect(true).to.equal(true);
+  });
+});
